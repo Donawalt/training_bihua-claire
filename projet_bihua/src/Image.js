d38a9d5cf2a843e9223ac3fe6a6a1ab6a05ce74f
Claire Fernandes <claire.fernandes@etu.univ-lyon1.fr>
2023-07-20 20:55:06 +0200

    défilement ralenti

--- projet_bihua/src/Image.js
+++ projet_bihua/src/Image.js
@@ -4,11 +4,18 @@ import logo from './image/img.png';
 export default function Image() {
   const sectionsRef = useRef([]);
   const [currentSection, setCurrentSection] = useState(0);
+  const [scrolling, setScrolling] = useState(false);
 
   useEffect(() => {
     const handleScroll = (event) => {
-      const direction = event.deltaY > 0 ? 1 : -1;
-      scrollToSection(currentSection + direction);
+      if (!scrolling) {
+        setScrolling(true);
+        const direction = event.deltaY > 0 ? 1 : -1;
+        scrollToSection(currentSection + direction);
+        setTimeout(() => {
+          setScrolling(false);
+        }, 800); // Réglez la durée du défilement ici (plus grand = plus lent)
+      }
     };
 
     const scrollToSection = (index) => {
@@ -22,7 +29,7 @@ export default function Image() {
 
     window.addEventListener('wheel', handleScroll);
     return () => window.removeEventListener('wheel', handleScroll);
-  }, [currentSection]);
+  }, [currentSection, scrolling]);
 
   return (
     <div className='container'>
@@ -35,28 +42,31 @@ export default function Image() {
 
             <td>
               <div className='scrollContainer'>
-                <div ref={(el) => (sectionsRef.current[0] = el)}>
+                <div ref={(el) => (sectionsRef.current[0] = el)} className='section'>
                   <h1 id="simple-list-item-1">Un parcours complet</h1>
                   <p>
                     En suivant l’un de nos parcours de 18 mois, vous allez relever des défis et repousser vos limites. Avec Tertia Via vous opérez une transformation en profondeur dans le but de devenir des leaders agiles, visionnaires, et inspirants, capables de réussir des changements majeurs et impactant pour vos activités et vos équipes.
                   </p>
-                  <button>Découvrir nos parcours<i className="bi bi-arrow-right"></i></button>
+                  <button>Découvrir nos parcours</button>
                 </div>
 
-                <div ref={(el) => (sectionsRef.current[1] = el)}>
-                  <h1 id="simple-list-item-2">Une expérience immersive et stimulante</h1>
+                <div ref={(el) => (sectionsRef.current[1] = el)} className='section'>
+                  <h1 id="simple-list-item-2">Un processus de transformation pour vous et votre activité</h1>
                   <p>
-                    En suivant l’un de nos parcours de 18 mois, vous allez relever des défis et repousser vos limites. Avec Tertia Via vous opérez une transformation en profondeur dans le but de devenir des leaders agiles, visionnaires, et inspirants, capables de réussir des changements majeurs et impactant pour vos activités et vos équipes.
+                    La finalité de nos programmes c’est de vous donner toutes les clés pour transformer votre organisation. Nous
+                    partons de votre projet et concrétisons sa transformation avec vous. Voilà pourquoi, pour accéder à notre
+                    enseignement, il est nécessaire d’avoir une situation professionnelle valide et concrète.
                   </p>
-                  <button>Découvrir nos parcours<i className="bi bi-arrow-right"></i></button>
+                  <button>Découvrir nos parcours</button>
                 </div>
 
-                <div ref={(el) => (sectionsRef.current[2] = el)}>
+                <div ref={(el) => (sectionsRef.current[2] = el)} className='section'>
                   <h1 id="simple-list-item-3">Une expérience immersive et stimulante</h1>
                   <p>
-                    En suivant l’un de nos parcours de 18 mois, vous allez relever des défis et repousser vos limites. Avec Tertia Via vous opérez une transformation en profondeur dans le but de devenir des leaders agiles, visionnaires, et inspirants, capables de réussir des changements majeurs et impactant pour vos activités et vos équipes.
+                    En suivant l’un de nos parcours de 18 mois, vous allez relever des défis et repousser vos limites. 
+                    Avec Tertia Via vous opérez une transformation en profondeur dans le but de devenir des leaders agiles, visionnaires, et inspirants, capables de réussir des changements majeurs et impactant pour vos activités et vos équipes.
                   </p>
-                  <button>Découvrir nos parcours<i className="bi bi-arrow-right"></i></button>
+                  <button>Découvrir nos parcours</button>
                 </div>
               </div>
             </td>
